4bc6d4fe636f80dc4afa65474a50c7a0ded04b9b
Jonathan Velasquez <johedvelca@gmail.com>
2025-05-11 23:54:00 -0400

    Sepolia not setup 3.13

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,11 @@
+//lshBYiAl1bWRiacGqd4btyMirPfVrwmG
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.0",
+  networks: {
+    url: 'https://eth-sepolia.g.alchemy.com/v2/pmzpT1090ebm06SqKD9ZHocqmQ6nGBij',
+    accounts: [ MetaMaskPK ]
+  }
 };
